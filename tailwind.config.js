e7632480678b62b33f723a26f131774cae437de8
zulu776 <zuluaga776@gmail.com>
2021-11-25 22:21:40 -0500

    segundo intento tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
+  purge: {
+    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"]
+    // These options are passed through directly to PurgeCSS
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
